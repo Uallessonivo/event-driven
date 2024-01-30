ef1cec2a4b7665bd860e32df1a0b3fe97365289f
Uallesson Nunes Ivo <uallessons@gmail.com>
2024-01-30 06:35:26 -0300

    Command component in CQRS & Event sourcing

--- twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,21 +1,26 @@
 package com.microservices.twitter.to.kafka.service;
 
 import com.microservices.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.twitter.to.kafka.service.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.microservices.twitter.to.kafka.service")
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+    private final StreamRunner streamRunner;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData, StreamRunner streamRunner) {
         this.twitterToKafkaServiceConfigData = configData;
+        this.streamRunner = streamRunner;
     }
 
     public static void main(String[] args) {
@@ -27,5 +32,6 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
         LOG.info("App starts...");
         LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
         LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+        streamRunner.start();
     }
 }
